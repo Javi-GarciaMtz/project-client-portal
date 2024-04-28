4986504af9d8da280c190a63c76415e6f44a00d6
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2024-04-28 08:30:23 -0600

    avances en la creacion de una solicitud

--- src/app/request/components/create-request/create-request.component.ts
+++ src/app/request/components/create-request/create-request.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RequestService } from '../../services/request/request.service';
+import { Subscription } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-request',
@@ -8,13 +10,33 @@ import { RequestService } from '../../services/request/request.service';
 })
 export class CreateRequestComponent {
 
-  constructor(
-    private _requestService: RequestService
-  ) {}
+  private arrSubs: Subscription[] = [];
+  public formCreateRequest: FormGroup;
+  public titleRequest: string = 'Constancia';
+  public typeForm: number = 1;
 
+  constructor(
+    private requestService: RequestService
+  ) {
+    this.formCreateRequest = new FormGroup({
+      type: new FormControl(1, [Validators.required]),
+    });
+
+  }
 
   onSubmit(): void {
-    this._requestService.setOption(1);
+    this.requestService.setOption(1);
+  }
+
+  changeTypeForm(): void {
+    const type = this.formCreateRequest.get('type')!.value;
+    this.typeForm = type;
+    if(type === 1) {
+      this.titleRequest = 'Constancia';
+    } else if(type === 2){
+      this.titleRequest = 'Dictamen';
+    }
+
   }
 
 }
